8677cddefcdc036c94aa89c6ea8d3f6dd22853d5
Evhen <malysh.evgeniy@gmail.com>
2023-11-12 20:11:20 +0200

    e1c05 - comment db conn timeout

--- src/model/store/mod.rs
+++ src/model/store/mod.rs
@@ -13,7 +13,7 @@ pub type Db = Pool<Postgres>;
 pub async fn new_db_pool() -> Result<Db> {
 	PgPoolOptions::new()
 		.max_connections(5)
-		.acquire_timeout(Duration::from_millis(500))
+		// .acquire_timeout(Duration::from_millis(500))
 		.connect(&config().DB_URL)
 		.await
 		.map_err(|ex| Error::FailToCreatePool(ex.to_string()))
